9e20f8823ead5f0fca258c600adc74d1b4e92575
Michala Nybroe <michalanim@gmail.com>
2021-09-29 22:05:35 +0200

    Initial commit

---
+++ src/main/java/com/example/htmlfun/HtmlFunApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlFunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/htmlfun/HtmlFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
